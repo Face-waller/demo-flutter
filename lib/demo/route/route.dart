01538f017f89cc31bf681e42e4b51f497ba6fcce
Administrator <2445530702@qq.com>
2021-07-18 14:17:44 +0800

    1

--- lib/demo/route/route.dart
+++ lib/demo/route/route.dart
@@ -3,43 +3,68 @@ import 'package:flutter/material.dart';
 class MyRouteTest extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('路由测试页面'),),
-      body: Wrap(
-        spacing:10,
-        runSpacing: 10,
-        runAlignment: WrapAlignment.center,
-        alignment: WrapAlignment.center,
-        children: [
-          ElevatedButton(
-            onPressed: (){
-              Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                return APage('B');
-              }));
-            },
-            child: Text('从A到B再返回')
-          ),
-          ElevatedButton(
+    return MaterialApp(
+      routes: {
+        "/A": (context)=> APage(''),
+        "/B": (context)=> BPage(),
+        "/C": (context)=> CPage(),
+        "/D": (context)=> DPage(),
+      },
+      home: Scaffold(
+        appBar: AppBar(title: Text('路由测试页面'),),
+        body: Wrap(
+          spacing:10,
+          runSpacing: 10,
+          runAlignment: WrapAlignment.start,
+          alignment: WrapAlignment.start,
+          children: [
+            ElevatedButton(
               onPressed: (){
-                Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                  return APage('C');
-                }));
+                Navigator.of(context).pushNamed("/B");
+              },
+              child: Text('从A到B再返回')
+            ),
+            ElevatedButton(
+              onPressed: (){
+                Navigator.of(context).pushNamed("/C");
               },
               child: Text('push代替pop')
-          ),
-        ],
+            ),
+            ElevatedButton(
+              onPressed: (){
+                Navigator.of(context).pushNamed("/D");
+              },
+              child: Text('pushReplacementNamed')
+            ),
+            ElevatedButton(
+              child: Text('maybePop'),
+              onPressed: () {
+                Navigator.of(context).maybePop();
+              }
+            ),
+            ElevatedButton(
+              child: Text('canPop'),
+              onPressed: () {
+                if (Navigator.canPop(context)) {
+                  print('可以pop');
+                  Navigator.of(context).pop();
+                }
+              }
+            ),
+          ],
+        ),
       ),
     );
   }
-
+  
 }
 class APage extends StatelessWidget {
   late final String goName;
-
+  
   APage(goName) {
     this.goName = goName;
   }
-
+  
   @override
   Widget build(BuildContext context) {
     String btName = '去往' + goName;
@@ -47,20 +72,26 @@ class APage extends StatelessWidget {
       appBar: AppBar(title: Text('A 页面'),),
       body: Container(
         alignment: Alignment.center,
-        child: ElevatedButton(
-          child: Text(btName),
-          onPressed: () {
-            Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-              switch (goName) {
-                case 'B':
-                  return BPage();
-                case 'C':
-                  return CPage();
-
+        child: Column(
+          children: [
+            ElevatedButton(
+              child: Text(btName),
+              onPressed: () {
+                switch (goName) {
+                  case 'B':
+                    Navigator.pushNamed(context, "/B");
+                    break;
+                  case 'C':
+                    Navigator.pushNamed(context, "/C");
+                    break;
+                  case 'D':
+                    Navigator.pushNamed(context, "/D");
+                    break;
+                }
+                Navigator.pushNamed(context, "/B");
               }
-              return BPage();
-            }));
-          },
+            ),
+          ],
         ),
       ),
     );
@@ -95,11 +126,38 @@ class CPage extends StatelessWidget {
         child: ElevatedButton(
           child: Text('去往 A'),
           onPressed: () {
-            Navigator.of(context).pop();
+            Navigator.pushNamed(context, "/A");
           },
         ),
       ),
     );
   }
 }
+
+class DPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('D 页面'),),
+      body: Column(
+        children: [
+          ElevatedButton(
+            child: Text('pushReplacementNamed 去往 B'),
+            onPressed: () {
+              Navigator.of(context).pushReplacementNamed("/B");
+            },
+          ),
+          ElevatedButton(
+            child: Text('pushReplacementNamed 去往 B'),
+            onPressed: () {
+              Navigator.of(context).popAndPushNamed("/B");
+            },
+          )
+        ],
+      ),
+    );
+  }
+  
+}
+
 
